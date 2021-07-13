c0dafc9de60aab03ab578d85e81522288fd7b1aa
pviriya <most43087sw3@gmail.com>
2021-07-13 21:47:30 +0100

    #5: Express starter structure

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Hello World");
+const mongoose = require('mongoose');
+const express = require('express');
+
+require('dotenv/config');
+
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+
+
+const app = express();
+const PORT = process.env.PORT || 7317;
+
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+);
+mongoose.connection.on('connected', () => {
+    console.log('Connected to Mongoo');
+    app.listen(PORT, () => {
+        console.log(`Lift Log Backend running of PORT ${PORT}`);
+    });
+});
