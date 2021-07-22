aabb11754857d82f4c368fdd6f93e5ed6827617a
pviriya <most54321@gmail.com>
2021-07-22 22:08:27 +0100

    #8: Update User Schema to use execise list

--- models/user.js
+++ models/user.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
+const exerciseSchema = mongoose.Schema({
+    name: { type: String, require: true }
+});
+
 const userSchema = mongoose.Schema({
     username: { type: String, require: true, unique: true },
     password: { type: String, require: true },
-    exercises: { type: [], default: [] },
+    exercises: { type: [exerciseSchema], default: [] },
     sessions: { type: [], default: [] },
 });
 
