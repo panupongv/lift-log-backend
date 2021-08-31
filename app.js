bb988df5c980c31d8619781cbe5c631d2212408c
pviriya <most54321@gmail.com>
2021-08-31 22:00:11 +0100

    #10: Create Workout (valid case) and test

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const authRoutes = require('./routes/authentication');
 const exercisesRoute = require('./routes/exercises');
 const sessionsRoute = require('./routes/sessions');
+const workoutsRoute = require('./routes/sessions-workouts');
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/:username/exercises', exercisesRoute);
 app.use('/api/:username/sessions', sessionsRoute);
+app.use('/api/:username/sessions', workoutsRoute);
 
 module.exports = app;
